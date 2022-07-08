0597a150ac98b35732fa32ffa7e7a33946286c96
Gen-Abdullah <adeoyeabdullahayomide@gmail.com>
2022-07-08 09:05:08 -0700

    Hot fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,26 +8,43 @@
  * Return: largest number
  */
 
-	int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c)
 {
+	a = 972;
+	b = -98;
+	c = 0;
 	int largest;
 
-	if (a >= b && a >= c)
-{
-	largest = a;
-}
-	else if (a >= b && c >= a)
-{
-	largest = c;
-}
-	else if (b >= c)
-{
-	largest = b;
-}
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
 	else
-{
+	{
 	largest = c;
-}
+	}
 
 	return (largest);
 }
+/**
+ * main - debugging example
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c,
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+
