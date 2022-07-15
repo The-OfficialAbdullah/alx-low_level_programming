a69aef9222ca06d20b9538ff34819d126a80bc08
Gen-Abdullah <adeoyeabdullahayomide@gmail.com>
2022-07-14 19:27:28 -0700

    strings copied

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - for comparing strings
+ * @s1: first parameter
+ * @s2: second parameter
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s2, s2);
+	return (result);
+}
