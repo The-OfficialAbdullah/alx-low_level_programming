17084cfeff761b441d7d4193c123795f465a0312
Gen-Abdullah <adeoyeabdullahayomide@gmail.com>
2022-07-21 19:24:21 -0700

    string length returned

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "mian.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	else
+		rerturn(1 + _strlen_recursion(s + 1));
+
+}
