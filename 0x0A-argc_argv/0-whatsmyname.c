fdc6749bfdaf84ef5d8fe35271ad32266162872c
Gen-Abdullah <adeoyeabdullahayomide@gmail.com>
2022-07-30 12:09:51 -0700

    head file corrected

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <sdtio.h>
+#include <stdio.h>
 
 /**
  * main - this is a function to print its name
@@ -6,7 +6,7 @@
  * @argv: an array of a command listed
  * Return: 0 for success
  */
-int main(int argc __attribute__((unused)), char *argv);
+int main(int argc __attribute__((unused)), char *argv)
 {
 	printf("%s\n", *argv);
 	return (0);
