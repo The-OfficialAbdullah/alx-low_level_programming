b721bffbc02adcf571acd94ea6353783b79d3a0d
Gen-Abdullah <adeoyeabdullahayomide@gmail.com>
2022-08-17 21:07:03 -0700

    task 3 fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,6 +3,7 @@
 /**
  * add_nodeint_end - adds a node to the end of a linked list
  * @head: pointer to the head of the list
+ * @n: number to be used as content
  *
  * Return: address of the newly added node
  */
