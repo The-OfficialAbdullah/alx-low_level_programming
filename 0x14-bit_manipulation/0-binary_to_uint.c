65baccbf2a5fcf7f37c8ae38661f4c1ca41547b5
Gen-Abdullah <adeoyeabdullahayomide@gmail.com>
2022-08-19 20:25:46 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * unsigned int.
  * @b: binary.
  *
- * Return: unsigned int
+ * Return: unsigned int.
  */
 unsigned int binary_to_unit(const char *b)
 {
@@ -19,6 +19,7 @@ unsigned int binary_to_unit(const char *b)
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
+
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
